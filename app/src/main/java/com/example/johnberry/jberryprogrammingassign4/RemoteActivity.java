1ffba100e066c503deaafb4ba1ab157a5e6a2ac6
AMDevG <jeberry308@gmail.com>
2017-10-12 00:31:48 -0500

    Init Commit

---
+++ app/src/main/java/com/example/johnberry/jberryprogrammingassign4/RemoteActivity.java
@@ -0,0 +1,19 @@
+package com.example.johnberry.jberryprogrammingassign4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class RemoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote);
+
+
+    }
+}
